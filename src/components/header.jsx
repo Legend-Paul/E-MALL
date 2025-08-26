089da2525ec3aca10e80882557381390e9660459
Legend Paul <qamunyamaina950@gmail.com>
2025-08-26 11:52:40 +0300

    Add search icon to the search bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
                             placehoder={"search item here"}
                         />
                         <Button
-                            label={"Search"}
+                            label={<i class="bi bi-search"></i>}
                             type={"submit"}
                             color={"none"}
                         />
